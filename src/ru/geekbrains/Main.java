e2931e24bd1b4b3a3eb8a0d9a60392f5555a58a9
Ekaterina Malygina <katty2007v@yandex.ru>
2020-11-14 22:04:44 +0300

    5 lesson. Final version.

--- src/ru/geekbrains/Main.java
+++ src/ru/geekbrains/Main.java
@@ -1,19 +1,36 @@
 package ru.geekbrains;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
 public class Main {
 
     public static final int CARS_COUNT = 4;
+
     public static void main(String[] args) {
+        CyclicBarrier cbStart = new CyclicBarrier(CARS_COUNT + 1);
+        CountDownLatch cdlStart = new CountDownLatch(1);
+        CountDownLatch cdlFinish = new CountDownLatch(CARS_COUNT);
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Race race = new Race(new Road(60), new Tunnel(CARS_COUNT / 2), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cbStart, cdlStart, cdlFinish);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        try {
+            cbStart.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            cdlStart.countDown();
+            cdlFinish.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/ru/geekbrains/Tunnel.java
+++ src/ru/geekbrains/Tunnel.java
@@ -1,24 +1,28 @@
 package ru.geekbrains;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
-    public Tunnel() {
+    private Semaphore smp;
+
+    public Tunnel(int lim) {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
+        smp = new Semaphore(lim);
     }
+
     @Override
     public void go(Car c) {
         try {
-            try {
-                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
-                System.out.println(c.getName() + " начал этап: " + description);
-                Thread.sleep(length / c.getSpeed() * 1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                System.out.println(c.getName() + " закончил этап: " + description);
-            }
+            System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+            smp.acquire();
+            System.out.println(c.getName() + " начал этап: " + description);
+            Thread.sleep(length / c.getSpeed() * 1000);
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            System.out.println(c.getName() + " закончил этап: " + description);
+            smp.release();
         }
     }
 }
